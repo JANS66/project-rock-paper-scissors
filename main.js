c45fd8b001c4e5c0fe14529845b92a3726b1ed3a
JANS <98973687+JANS66@users.noreply.github.com>
2025-07-03 11:16:52 +0300

    feat: playGame function will announce score after each round

--- main.js
+++ main.js
@@ -26,25 +26,32 @@ function playGame() {
             let computerChoice = getComputerChoice();
 
             if (humanChoice === computerChoice) {
-                return "Tie!";
+                console.log("Tie!");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "rock" && computerChoice === "scissors") {
                 humanScore++;
-                return "You win! Rock beats scissors";
+                console.log("You win! Rock beats scissors");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "rock" && computerChoice === "paper") {
                 computerScore++;
-                return "You lose! Paper beats rock";
+                console.log("You lose! Paper beats rock");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "paper" && computerChoice === "rock") {
                 humanScore++;
-                return "You win! Paper beats rock";
+                console.log("You win! Paper beats rock");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "paper" && computerChoice === "scissors") {
                 computerScore++;
-                return "You lose! Scissors beat paper";
+                console.log("You lose! Scissors beat paper");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "scissors" && computerChoice === "rock") {
                 computerScore++;
-                return "You lose! Rock beat scissors";
+                console.log("You lose! Rock beat scissors");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             } else if (humanChoice === "scissors" && computerChoice === "paper") {
                 humanScore++;
-                return "You win! Scissors beat paper";
+                console.log("You win! Scissors beat paper");
+                return `Human ${humanScore} - ${computerScore} Computer`;
             }
         }
     }
